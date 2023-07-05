fbe3ef3ba5d703563193c2e067ce5a1d6857a2d9
Andy5424 <138640129+Andy5424@users.noreply.github.com>
2023-07-04 22:11:39 -0500

    Add files via upload

---
+++ java/js/prueba.js
@@ -0,0 +1,33 @@
+function operar(e){
+  var n1= parseInt(document.getElementById('nota_1').value);
+  var n2= parseInt(document.getElementById('nota_2').value);
+  var n3= parseInt(document.getElementById('nota_3').value);
+  if(document.getElementById('nota_1').value == 0||document.getElementById('nota_2').value == 0||document.getElementById('nota_3').value == 0){
+
+  }else{
+    k=(n1+n2+n3)/3;
+
+    alert("tu promedio es "+""+k);
+    
+  }
+  
+  
+ 
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
